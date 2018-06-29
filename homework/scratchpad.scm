c94f017f7904f958d1b931cced5eb7062507f06d
Deep-Red <kirbycolebrown@gmail.com>
2018-06-28 23:20:12 -0400

    Setup for week12.

--- homework/scratchpad.scm
+++ homework/scratchpad.scm
@@ -986,3 +986,21 @@
       new-list
       (build-list (cons (stream-ref stream ref) new-list) (- ref 1))))
   (build-list '() (- numdigits 1)))
+
+; The Metacircular Evaluator
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+    ((variable? exp) (lookup-variable-value exp env))
+    ((quoted? exp) (text-of-quotation exp))
+    ((assignment? exp) (eval-assignment exp env))
+    ((definition? exp) (eval-definition exp env))
+    ((if? exp) (eval-if exp env))
+    ((lambda? exp) (make-procedure (lambda-parameters exp) (lambda-body exp) env))
+    ((begin? exp) (eval-sequence (begin actions exp) env))
+    ((cond? exp) (eval (cond->if exp) env))
+    ((application? exp) (apply (eval (operator exp) env) (list-of-values (operands exp) env)))
+    (else (error "Unknown expression type: EVAL" exp))))
+
+(define (eval-dd exp env)
+  )
---
+++ homework/week12.md
@@ -0,0 +1,7 @@
+# 1
+
+*Abelson & Sussman, exercises 4.3, 4.6, 4.7, 4.10, 4.11, 4.13, 4.14, 4.15*
+
+## 4.3
+
+*Rewrite `eval` so that the dispatch is done in data-directed style. Compare this with the data-directed differentiation procedure of Exercise 2.73. (You may use the `car` of a compound expression as the type of the expression, as is appropriate for the syntax implemented in this section.)
